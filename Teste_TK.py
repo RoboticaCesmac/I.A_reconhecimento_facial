c91c969fa3b9da4fdc3d5d4820b20c4f471b58d1
Alvaro <alvaro2014oliveira@gmail.com>
2021-10-15 15:31:57 -0300

    Atualização porta

--- Teste_TK.py
+++ Teste_TK.py
@@ -1,34 +1,38 @@
-#coding: utf8
-import sys
-from tkinter import *
-from PIL import Image, ImageTk 
+# #coding: utf8
+# import sys
+# from tkinter import *
+# from PIL import Image, ImageTk 
 
 
-def sair():
-    sys.exit()
+# def sair():
+#     sys.exit()
 
 
-janela = Tk()
-janela.attributes("-fullscreen", True)
+# janela = Tk()
+# janela.attributes("-fullscreen", True)
 
-janela.title("Teste TK")
-
-
-
-tkimage = ImageTk.PhotoImage(Image.open("TEST.png"))
-imagem = Label( text = "adicionando", image = tkimage)
-imagem.image = tkimage
-imagem.pack(padx=20, pady=20)
+# janela.title("Teste TK")
 
 
 
-bt = Button(janela, width=20, text="Fechar", command = sair)
-
-bt.pack(side=BOTTOM, padx=20, pady=20)
-
-janela.geometry("800x700+290+0")
-
-
-janela.mainloop()
+# tkimage = ImageTk.PhotoImage(Image.open("TEST.png"))
+# imagem = Label( text = "adicionando", image = tkimage)
+# imagem.image = tkimage
+# imagem.pack(padx=20, pady=20)
 
 
+
+# bt = Button(janela, width=20, text="Fechar", command = sair)
+
+# bt.pack(side=BOTTOM, padx=20, pady=20)
+
+# janela.geometry("800x700+290+0")
+
+
+# janela.mainloop()
+
+
+import tensorflow as tf 
+tf.test.is_gpu_available(
+    cuda_only=False, min_cuda_compute_capability=None
+)
